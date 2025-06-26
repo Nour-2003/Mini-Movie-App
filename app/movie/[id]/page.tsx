4d6d488cd65f28b4a1630e724622a21d05dc263c
Nour-2003 <noureldeanh@gmail.com>
2025-06-27 02:53:13 +0300

    testing params issue

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -51,12 +51,11 @@ interface Video {
   official: boolean;
 }
 
-interface PageProps {
-  params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-}
-
-export async function generateMetadata({ params }: PageProps) {
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
   const movie = await fetchMovie(id);
 
